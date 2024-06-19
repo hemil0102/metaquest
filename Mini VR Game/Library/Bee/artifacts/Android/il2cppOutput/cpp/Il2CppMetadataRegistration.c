4ffafb3e20e0781cd1d86fd864aebefa65b0eafc
Harry Ho <hemil0102@gmail.com>
2024-06-19 16:39:43 +0900

    track large files with LFS

---
+++ Mini VR Game/Library/Bee/artifacts/Android/il2cppOutput/cpp/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	11352,
+	g_Il2CppGenericTypes,
+	7252,
+	g_Il2CppGenericInstTable,
+	97959,
+	g_Il2CppGenericMethodFunctions,
+	39141,
+	g_Il2CppTypeTable,
+	115643,
+	g_Il2CppMethodSpecTable,
+	12868,
+	g_FieldOffsetTable,
+	12868,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
